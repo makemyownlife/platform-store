6b53e8c06108d816069e0a53cb6acdbee5d0d23d
zhangyong <zhangyong7120180@163.com>
2020-05-07 20:15:15 +0800

    [修改代码] 添加RefereanceResource storeUtil

---
+++ platform-store-core/src/main/java/com/courage/platform/store/core/StoreUtil.java
@@ -0,0 +1,20 @@
+package com.courage.platform.store.core;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+
+public class StoreUtil {
+
+    public static final long TOTAL_PHYSICAL_MEMORY_SIZE = getTotalPhysicalMemorySize();
+
+    @SuppressWarnings("restriction")
+    public static long getTotalPhysicalMemorySize() {
+        long physicalTotal = 1024 * 1024 * 1024 * 24L;
+        OperatingSystemMXBean osmxb = ManagementFactory.getOperatingSystemMXBean();
+        if (osmxb instanceof com.sun.management.OperatingSystemMXBean) {
+            physicalTotal = ((com.sun.management.OperatingSystemMXBean) osmxb).getTotalPhysicalMemorySize();
+        }
+
+        return physicalTotal;
+    }
+}
