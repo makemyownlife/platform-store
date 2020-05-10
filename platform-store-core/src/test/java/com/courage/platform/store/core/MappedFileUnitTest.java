2315fa6ffbbb3af397fe4f5faa8df11ed28e3ee3
zhangyong <zhangyong7120180@163.com>
2020-05-10 10:52:00 +0800

    mappedfile szie

---
+++ platform-store-core/src/test/java/com/courage/platform/store/core/MappedFileUnitTest.java
@@ -0,0 +1,42 @@
+package com.courage.platform.store.core;
+
+import com.courage.platform.store.core.util.PlatformUtilAll;
+import org.junit.After;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MappedFileUnitTest {
+
+    private final String storeMessage = "Once, there was a chance for me!";
+
+    @Test
+    public void testSelectMappedBuffer() throws IOException {
+        MappedFile mappedFile = new MappedFile("target/unit_test_store/MappedFileTest/000", 1024 * 64);
+        boolean result = mappedFile.appendMessage(storeMessage.getBytes());
+        assertThat(result).isTrue();
+
+        SelectMappedBufferResult selectMappedBufferResult = mappedFile.selectMappedBuffer(0);
+        byte[] data = new byte[storeMessage.length()];
+        selectMappedBufferResult.getByteBuffer().get(data);
+        String readString = new String(data);
+
+        assertThat(readString).isEqualTo(storeMessage);
+
+        mappedFile.shutdown(1000);
+        assertThat(mappedFile.isAvailable()).isFalse();
+        selectMappedBufferResult.release();
+        assertThat(mappedFile.isCleanupOver()).isTrue();
+        assertThat(mappedFile.destroy(1000)).isTrue();
+    }
+
+    @After
+    public void destory() {
+        File file = new File("target/unit_test_store");
+        PlatformUtilAll.deleteFile(file);
+    }
+
+}
--- platform-store-core/src/test/java/com/courage/platform/store/core/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by zhangyong on 2020/5/10.
- */
-package com.courage.platform.store.core;
