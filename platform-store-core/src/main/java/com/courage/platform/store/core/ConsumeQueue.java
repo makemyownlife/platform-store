82858e78d4a18f28c1a8ec459e22472d01b531e1
zhangyong <zhangyong7120180@163.com>
2020-05-11 20:04:51 +0800

    [修改代码] consumer 定义大小

--- platform-store-core/src/main/java/com/courage/platform/store/core/ConsumeQueue.java
+++
@@ -1,8 +0,0 @@
-package com.courage.platform.store.core;
-
-
-public class ConsumeQueue {
-
-    public static final int CQ_STORE_UNIT_SIZE = 20;
-
-}
---
+++ platform-store-core/src/main/java/com/courage/platform/store/core/domain/constant/ConsumerQueueConstants.java
@@ -0,0 +1,11 @@
+package com.courage.platform.store.core.domain.constant;
+
+/**
+ * 存储单元大小
+ * Created by zhangyong on 2020/5/11.
+ */
+public class ConsumerQueueConstants {
+
+    public static final int CQ_STORE_UNIT_SIZE = 20;
+
+}
