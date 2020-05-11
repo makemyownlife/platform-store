4bc60c3a417d41c31eb1b265018e2c0e7f531b32
zhangyong <zhangyong7120180@163.com>
2020-05-11 19:23:00 +0800

    [修改代码] 单元测试信息

---
+++ platform-store-core/src/test/java/com/courage/platform/store/core/MappedFileQueueUnitTest.java
@@ -0,0 +1,25 @@
+package com.courage.platform.store.core;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MappedFileQueueUnitTest {
+
+    @Test
+    public void testGetLastMappedFile() {
+        final String fixedMsg = "0123456789abcdef";
+
+        MappedFileQueue mappedFileQueue = new MappedFileQueue("target/unit_test_store/a/", 1024, null);
+
+        for (int i = 0; i < 1024; i++) {
+            MappedFile mappedFile = mappedFileQueue.getLastMappedFile(0);
+            assertThat(mappedFile).isNotNull();
+            assertThat(mappedFile.appendMessage(fixedMsg.getBytes())).isTrue();
+        }
+
+        mappedFileQueue.shutdown(1000);
+        mappedFileQueue.destroy();
+    }
+
+}
