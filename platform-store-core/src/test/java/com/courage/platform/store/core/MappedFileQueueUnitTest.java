9efa02aaccaabde6793fafca32b7e3fde89137fd
zhangyong <zhangyong7120180@163.com>
2020-05-22 20:57:36 +0800

    [添加代码] 添加hashwheeltimer

--- platform-store-core/src/test/java/com/courage/platform/store/core/MappedFileQueueUnitTest.java
+++ platform-store-core/src/test/java/com/courage/platform/store/core/MappedFileQueueUnitTest.java
@@ -9,17 +9,6 @@ public class MappedFileQueueUnitTest {
     @Test
     public void testGetLastMappedFile() {
         final String fixedMsg = "0123456789abcdef";
-
-        MappedFileQueue mappedFileQueue = new MappedFileQueue("target/unit_test_store/a/", 1024, null);
-
-        for (int i = 0; i < 1024; i++) {
-            MappedFile mappedFile = mappedFileQueue.getLastMappedFile(0);
-            assertThat(mappedFile).isNotNull();
-            assertThat(mappedFile.appendMessage(fixedMsg.getBytes())).isTrue();
-        }
-
-        mappedFileQueue.shutdown(1000);
-        mappedFileQueue.destroy();
     }
 
 }
