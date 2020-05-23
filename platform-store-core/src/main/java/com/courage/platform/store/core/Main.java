6c171b67e2b70626fe62c12fb4a2b1bde3c00de8
zhangyong <zhangyong7120180@163.com>
2020-05-23 11:33:52 +0800

    相关的索引文件

---
+++ platform-store-core/src/main/java/com/courage/platform/store/core/Main.java
@@ -0,0 +1,10 @@
+package com.courage.platform.store.core;
+
+/**
+ * Created by zhangyong on 2020/5/23.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
